80d603aedb03e0d92685d63ed976b287b8262709
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-07-14 14:51:17 +0200

    mod from all to paginate for postman

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::paginate(5);
 
         return response()->json($projects);
     }
